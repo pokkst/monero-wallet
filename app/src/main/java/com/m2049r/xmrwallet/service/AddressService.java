67ab38d91f6a865f57ee891857ee957e368f0d12
pokkst <pokkst@protonmail.com>
2022-09-10 17:08:52 -0500

    i think for 1.0 i won't include a subaddress list/screen, so remove these methods for now

--- app/src/main/java/com/m2049r/xmrwallet/service/AddressService.java
+++ app/src/main/java/com/m2049r/xmrwallet/service/AddressService.java
@@ -4,6 +4,8 @@ import com.m2049r.xmrwallet.data.Subaddress;
 import com.m2049r.xmrwallet.model.TransactionInfo;
 import com.m2049r.xmrwallet.model.Wallet;
 import com.m2049r.xmrwallet.model.WalletManager;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AddressService extends ServiceBase {
@@ -13,7 +15,6 @@ public class AddressService extends ServiceBase {
         return instance;
     }
 
-    private final HashMap<String, Integer> subAddresses = new HashMap<>();
     private int latestAddressIndex = 1;
 
     public AddressService(MoneroHandlerThread thread) {
@@ -22,16 +23,6 @@ public class AddressService extends ServiceBase {
     }
 
     public void refreshAddresses() {
-        Wallet wallet = WalletManager.getInstance().getWallet();
-        int issuedAddressesSize = WalletManager.getInstance().getWallet().getNumSubaddresses();
-        if(subAddresses.size() != issuedAddressesSize) {
-            for (int i = 0; i < issuedAddressesSize; i++) {
-                if(!subAddresses.containsValue(i)) {
-                    subAddresses.put(wallet.getSubaddress(i), i);
-                }
-            }
-        }
-
         for (TransactionInfo info : HistoryService.getInstance().getHistory()) {
             if(info.addressIndex > latestAddressIndex) {
                 latestAddressIndex = info.addressIndex;
@@ -46,9 +37,5 @@ public class AddressService extends ServiceBase {
     public Subaddress getLatestSubaddress() {
         Wallet wallet = WalletManager.getInstance().getWallet();
         return wallet.getSubaddressObject(latestAddressIndex);
-    }
-
-    public HashMap<String, Integer> getIssuedSubaddresses() {
-        return subAddresses;
     }
 }
